7a3bfaebb5efbfb33a61e4d7a74f135efb978d24
Shaharyaar <73778412+shaharyaarzaye@users.noreply.github.com>
2024-10-20 09:09:42 +0000

    created the link list and inserted the node and also printed  the list

--- Linked list/creationAndIntertion.cpp
+++ Linked list/creationAndIntertion.cpp
@@ -1,34 +1,56 @@
-#include<stdio.h>
-#include<iostream>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>  // Needed for malloc
+
 
 struct node {
     int data;
-    struct node * next;
+    struct node* link;
 };
 
-// Creation // printing // insertion at begning // insertion at random // intertion at the end // deletion at begning // deletion at random // deletion at the end // reversal of linked list 
+struct node* head = nullptr; // Initialize head to nullptr
+struct node* newNode;
 
-struct node * head ;
-
-
-void intertion( int d ){
-    struct node * newNode ;
-    newNode = (struct node *)malloc(sizeof(struct node));
-    newNode->data = d;
-    newNode->next = NULL;
-    head = newNode;
-    printf("%d \n" , head->data);
-
-};
-
-void deletion(int pos){
-    int i = 1;
-    struct node * temp ;
-    temp = head;
+void insertNode(int n) {
+    newNode = (struct node*)malloc(sizeof(struct node));
+    struct node* temp;
+    newNode->data = n;
+    newNode->link = nullptr;
+    
+    if (head == nullptr) { // Check if head is null
+        head = newNode;
+    } 
+    else{
+        temp = head ;
+        while(temp->link != 0){
+            temp = temp->link;
+        };
+        temp->link = newNode;
+    };
+    
 }
 
+void printList(){
+   struct node* temp = head; // Initialize temp with head
+    while (temp != nullptr) { // Iterate until temp is nullptr
+        printf("%d ", temp->data); // Print data
+        printf("%d ", &temp->data); // Print the address of the memory
+        temp = temp->link; // Move to the next node
+    }
+    printf("\n"); // Print newline after the list
 
-int main(){
-   
+}
+
+int main() {
+    int choice = 1;
+    while (choice != 0) { // Corrected to check for 0
+        int n;
+        printf("Enter the number you want to add to the list: ");
+        scanf("%d", &n);
+        insertNode(n);
+        printf("If you want to stop adding press 0, otherwise press any other number: ");
+        scanf("%d", &choice);
+    }
+    printList();
+
+    return 0;
 }
