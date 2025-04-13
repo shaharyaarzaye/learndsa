a6844d4f09f6ed01e5af1fe6b6d6769d5a3f2c18
Shaharyaar <73778412+shaharyaarzaye@users.noreply.github.com>
2025-04-13 12:07:54 +0000

    implemented insertion and DFS algorithm

--- Trees/Binary_Tree.py
+++ Trees/Binary_Tree.py
@@ -4,3 +4,13 @@ class TreeNode:
         self.left = None
         self.right = None
 
+    def search(root , target):
+    if not root:
+        return False
+    
+    if target > root.val:
+        return search(root.right , target)
+    elif target < root.val:
+        return search(root.left, target)
+    else
+        return True
--- Trees/DFS.py
+++ Trees/DFS.py
@@ -29,9 +29,38 @@ class BST:
             else:
                 self._insert(current_node.right, value)
 
+    # DFS Traversal (in-order, pre-order, post-order)
+
+    # In-order (Left, Root, Right)
+    def inorder(self, node):
+        if node:
+            self.inorder(node.left)
+            print(node.value, end=' ')
+            self.inorder(node.right)
+
+    # Pre-order (Root, Left, Right)
+    def preorder(self, node):
+        if node:
+            print(node.value, end=' ')
+            self.preorder(node.left)
+            self.preorder(node.right)
+
+    # Post-order (Left, Right, Root)
+    def postorder(self, node):
+        if node:
+            self.postorder(node.left)
+            self.postorder(node.right)
+            print(node.value, end=' ')
 
 # Usage
 bst = BST()
 values = [50, 30, 70, 20, 40, 60, 80]
 for val in values:
     bst.insert(val)
+
+print("Inorder traversal:")
+bst.inorder(bst.root)
+print("\nPreorder traversal:")
+bst.preorder(bst.root)
+print("\nPostorder traversal:")
+bst.postorder(bst.root)
